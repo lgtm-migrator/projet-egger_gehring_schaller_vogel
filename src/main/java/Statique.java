bde337c2ba1c4ddee29a397497811ff316b45e4e
jorisSchaller <joris.schaller@heig-vd.ch>
2022-03-22 17:41:02 +0100

    fix l'indentation

--- src/main/java/Statique.java
+++ src/main/java/Statique.java
@@ -1,14 +1,13 @@
-import java.util.Properties;
-import java.util.concurrent.Callable;
-
 import commands.*;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.util.concurrent.Callable;
+
 @Command(
         name = "statique",
         description = "A brand new static site generator.",
-        subcommands = {Init.class, Clean.class, Build.class, Serve.class,Version.class})
+        subcommands = {Init.class, Clean.class, Build.class, Serve.class, Version.class})
 public class Statique implements Callable<Integer> {
     public static void main(String... args) {
         int exitCode = new CommandLine(new Statique()).execute(args);
--- src/main/java/commands/Version.java
+++ src/main/java/commands/Version.java
@@ -1,21 +1,23 @@
 package commands;
 
-        import java.util.Properties;
-        import java.util.concurrent.Callable;
-        import picocli.CommandLine.Command;
+import picocli.CommandLine.Command;
+
+import java.util.Properties;
+import java.util.concurrent.Callable;
 
 @Command(name = "-version", description = "Prints the version of the program")
 public class Version implements Callable<Integer> {
-    @Override public Integer call() {
+    @Override
+    public Integer call() {
         try {
             //create a new property file
             final Properties properties = new Properties();
             //load the one we store the version in
             properties.load(Version.class.getClassLoader().getResourceAsStream("project.properties"));
             //get the version
-            System.out.println("Statique :\tv"+ properties.getProperty("version"));
-        }catch (Exception e){
-            System.out.println("Error while reading version number:\n"+e.getMessage());
+            System.out.println("Statique :\tv" + properties.getProperty("version"));
+        } catch (Exception e) {
+            System.out.println("Error while reading version number:\n" + e.getMessage());
             return -1;
         }
         return 0;
