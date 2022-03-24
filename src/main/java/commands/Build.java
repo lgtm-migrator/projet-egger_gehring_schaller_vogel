4a018996ffb307c83acd72adb598be89d4db01d9
maelle <maelle.vogel@heig-vd.ch>
2022-03-24 12:45:54 +0100

    Une fois appliqué ce commit permet de vérifier que le dossier existe et que c'est bien un dossier

--- src/main/java/commands/Build.java
+++ src/main/java/commands/Build.java
@@ -1,13 +1,35 @@
 
 package commands;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import java.io.File;
 
 @Command(name = "build", description = "Build a static site")
 public class Build implements Callable<Integer> {
 
+    @CommandLine.Parameters(paramLabel = "rootDirectory", description = "root directory of markdown files")
+    public File rootDirectory;
+
     @Override public Integer call() throws IOException {
+        if(rootDirectory.exists()){
+            if(rootDirectory.isDirectory()){
+                String[] file = rootDirectory.list();
+                assert file != null;
+                System.out.println("OKAY");
+
+            }else{
+                System.out.println("Error: not a directory");
+            }
+
+        }else{
+            System.out.println("Error: incorrect directory");
+        }
+
+
         return 0;
     }
 
