3d08aeeeb471208af380e59ab74898d6439f57e1
maelle <maelle.vogel@heig-vd.ch>
2022-03-31 18:05:39 +0200

    formatage
    Signed-off-by: maelle <maelle.vogel@heig-vd.ch>

--- src/main/java/commands/Build.java
+++ src/main/java/commands/Build.java
@@ -11,74 +11,74 @@ import picocli.CommandLine.Command;
 @Command(name = "build", description = "Build a static site")
 public class Build implements Callable<Integer> {
 
-  @CommandLine.Parameters(
-      paramLabel = "rootDirectory",
-      description = "root directory of markdown files")
-  public File rootDirectory;
+    @CommandLine.Parameters(
+            paramLabel = "rootDirectory",
+            description = "root directory of markdown files")
+    public File rootDirectory;
 
-  @Override
-  public Integer call() throws IOException {
-    // check param exists
-    if (rootDirectory == null) {
-      throw new NullPointerException("Le nom de dossier ne peut pas être vide");
-    }
-
-    if (rootDirectory.exists()) {
-      if (rootDirectory.isDirectory()) {
-
-        String[] file = rootDirectory.list();
-
-        assert file != null;
-        // the directory must not be empty
-        if (file.length == 0) {
-          System.out.println("Error: Empty directory");
-          return -1;
+    @Override
+    public Integer call() throws IOException {
+        // check param exists
+        if (rootDirectory == null) {
+            throw new NullPointerException("Le nom de dossier ne peut pas être vide");
         }
 
-        // create dir build
-        Files.createDirectories(Path.of(rootDirectory + "/build"));
+        if (rootDirectory.exists()) {
+            if (rootDirectory.isDirectory()) {
 
-        // read each file
-        for (String md : file) {
-          File md_file = new File(rootDirectory + "/" + md);
+                String[] file = rootDirectory.list();
 
-          // only parse if it's a file
-          if (md_file.isFile()) {
+                assert file != null;
+                // the directory must not be empty
+                if (file.length == 0) {
+                    System.out.println("Error: Empty directory");
+                    return -1;
+                }
 
-            String name = md_file.getName();
-            int ext = name.lastIndexOf('.');
-            String extension = name.substring(ext);
+                // create dir build
+                Files.createDirectories(Path.of(rootDirectory + "/build"));
 
-            // check extension is md
-            if (extension.equals(".md")) {
-              String fileNameWithOutExt = name.substring(0, ext);
+                // read each file
+                for (String md : file) {
+                    File md_file = new File(rootDirectory + "/" + md);
 
-              Runtime runtime = Runtime.getRuntime();
-              String command =
-                  "pandoc --standalone -o "
-                      + rootDirectory
-                      + "/build/"
-                      + fileNameWithOutExt
-                      + ".html "
-                      + rootDirectory
-                      + "/"
-                      + fileNameWithOutExt
-                      + ".md";
-              runtime.exec(command);
+                    // only parse if it's a file
+                    if (md_file.isFile()) {
+
+                        String name = md_file.getName();
+                        int ext = name.lastIndexOf('.');
+                        String extension = name.substring(ext);
+
+                        // check extension is md
+                        if (extension.equals(".md")) {
+                            String fileNameWithOutExt = name.substring(0, ext);
+
+                            Runtime runtime = Runtime.getRuntime();
+                            String command =
+                                    "pandoc --standalone -o "
+                                            + rootDirectory
+                                            + "/build/"
+                                            + fileNameWithOutExt
+                                            + ".html "
+                                            + rootDirectory
+                                            + "/"
+                                            + fileNameWithOutExt
+                                            + ".md";
+                            runtime.exec(command);
+                        }
+                    }
+                }
+
+            } else {
+                System.out.println("Error: not a directory");
+                return -1;
             }
-          }
+
+        } else {
+            System.out.println("Error: incorrect directory");
+            return -1;
         }
 
-      } else {
-        System.out.println("Error: not a directory");
-        return -1;
-      }
-
-    } else {
-      System.out.println("Error: incorrect directory");
-      return -1;
+        return 0;
     }
-
-    return 0;
-  }
 }
