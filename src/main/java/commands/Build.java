ebf849ac4beedfad8ac7e52d68a57303b134244f
maelle <maelle.vogel@heig-vd.ch>
2022-03-31 12:16:50 +0200

    Parcours les fichiers et ne garde que les .md

--- src/main/java/commands/Build.java
+++ src/main/java/commands/Build.java
@@ -1,12 +1,14 @@
 
 package commands;
 import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import java.io.File;
+
 
 @Command(name = "build", description = "Build a static site")
 public class Build implements Callable<Integer> {
@@ -19,6 +21,27 @@ public class Build implements Callable<Integer> {
             if(rootDirectory.isDirectory()){
                 String[] file = rootDirectory.list();
                 assert file != null;
+                
+                for( String md : file){
+                    File md_file = new File(rootDirectory + "/" + md);
+
+                    //only parse if it's a file
+                    if(md_file.isFile()){
+
+                        String name = md_file.getName();
+                        int ext = name.lastIndexOf('.');
+                        String extension = name.substring(ext);
+
+                        //check extension is md
+                        if(extension.equals(".md")) {
+                            String fileNameWithOutExt = name.substring(0, ext);
+                            System.out.println("nom: " + fileNameWithOutExt + " extension: " + extension);
+
+
+                        }
+
+                    }
+                }
                 System.out.println("OKAY");
 
             }else{
