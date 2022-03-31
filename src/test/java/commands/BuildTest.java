be9484b1b0dc5a7947dfda093b856bbf8d27deaa
maelle <maelle.vogel@heig-vd.ch>
2022-03-31 17:56:11 +0200

    création dans les tests de before et after all pour éviter de push des dossiers inutiles.
    Signed-off-by: maelle <maelle.vogel@heig-vd.ch>

--- src/test/java/commands/BuildTest.java
+++ src/test/java/commands/BuildTest.java
@@ -4,17 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class BuildTest {
+  private static final String TEST_PATH = "test1/";
+
   private static final String TEST_PATH_1 = "test1/site/";
 
   // not existing directory
   private static final String TEST_PATH_2 = "test2/site/";
 
   // empty directory
-  private static final String TEST_PATH_3 = "test3/site/";
+  private static final String TEST_PATH_3 = "test3/";
+
+  @BeforeAll
+  public static void CreateDirectory() throws IOException {
+    Files.createDirectories(Path.of("test1/"));
+    Files.createDirectories(Path.of(TEST_PATH_1));
+    Files.createFile(Path.of(TEST_PATH_1 + "fichier2.md"));
+    Files.createFile(Path.of(TEST_PATH_1 + "test.md"));
+    Files.createDirectories(Path.of(TEST_PATH_3));
+  }
 
   @Test
   public void testBuildWithoutParameterThrowsException() {
@@ -36,7 +51,7 @@ public class BuildTest {
   public void testBuildOnDirectoryThatDoesntExistsDoesntWork() throws IOException {
     Build build = new Build();
     build.rootDirectory = new File(TEST_PATH_2);
-    // assert que build retourne sans erreur
+    // assert que build retourne avec erreur
     assertEquals(build.call(), -1);
   }
 
@@ -44,7 +59,7 @@ public class BuildTest {
   public void testBuildOnFileDoesntWork() throws IOException {
     Build build = new Build();
     build.rootDirectory = new File(TEST_PATH_1 + "/test.md");
-    // assert que build retourne sans erreur
+    // assert que build retourne avec erreur
     assertEquals(build.call(), -1);
   }
 
@@ -52,7 +67,7 @@ public class BuildTest {
   public void testBuildOnEmptyDirectoryDoesntWork() throws IOException {
     Build build = new Build();
     build.rootDirectory = new File(TEST_PATH_3);
-    // assert que build retourne sans erreur
+    // assert que build retourne avec erreur
     assertEquals(build.call(), -1);
   }
 
@@ -60,7 +75,7 @@ public class BuildTest {
   public void testBuildOnInvalidParameterDoesntWork() throws IOException {
     Build build = new Build();
     build.rootDirectory = new File("*qwert");
-    // assert que build retourne sans erreur
+    // assert que build retourne avec erreur
     assertEquals(build.call(), -1);
   }
 
@@ -88,5 +103,23 @@ public class BuildTest {
     Arrays.sort(result);
     String[] except = {"fichier2.html", "test.html"};
     assertArrayEquals(result, except);
+  }
+
+  @AfterAll
+  public static void deleteDirectories() {
+    File directory1 = new File(TEST_PATH);
+    deleteDirectory(directory1);
+    File directory2 = new File(TEST_PATH_3);
+    deleteDirectory(directory2);
+  }
+
+  private static void deleteDirectory(File directory) {
+    File[] allContents = directory.listFiles();
+    if (allContents != null) {
+      for (File file : allContents) {
+        deleteDirectory(file);
+      }
+    }
+    directory.delete();
   }
 }
