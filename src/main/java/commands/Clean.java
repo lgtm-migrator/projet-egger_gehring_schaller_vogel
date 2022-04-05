6e6626f3b0792fb4d8bc8a1af3331d60caa137b7
magskwa <magali.egger@heig-vd.ch>
2022-04-05 09:26:46 +0200

    ajout de la commande clean

--- src/main/java/commands/Clean.java
+++ src/main/java/commands/Clean.java
@@ -1,14 +1,49 @@
 package commands;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "clean", description = "Clean a static site")
 public class Clean implements Callable<Integer> {
 
-    @Override public Integer call() throws IOException {
+    @Parameters(paramLabel = "SITE", description = "The site to build")
+    public Path site;
+
+    @Override
+    public Integer call() throws IOException {
+        if (site == null) {
+            throw new NullPointerException("site ne doit pas etre null");
+        }
+        Path path = Paths.get( System.getProperty("user.dir") + site.toString() + "//build");
+        File file = path.toFile();
+
+        if (!file.exists() || !file.isDirectory()) {
+            return -1;
+        }
+
+        delete(file);
         return 0;
+
+
     }
 
+    private void delete(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    delete(entry);
+
+                }
+            }
+        }
+        if (!file.delete()) {
+            throw new IOException("Failed to delete " + file);
+        }
+    }
 }
